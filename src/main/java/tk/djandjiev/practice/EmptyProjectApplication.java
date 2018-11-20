7ee32976e658463393f44f61b6a1e074908dbe91
Victor Djandjiev <victor.d.aka.spleen@gmail.com>
2018-11-20 13:15:11 +0300

    first commit

---
+++ src/main/java/tk/djandjiev/practice/EmptyProjectApplication.java
@@ -0,0 +1,12 @@
+package tk.djandjiev.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptyProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EmptyProjectApplication.class, args);
+  }
+}
